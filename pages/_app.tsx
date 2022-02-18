88439def1d98ed0719071ebd41f92139d6114f82
Chihab Jraoui <contact@chihabjraoui.com>
2022-02-19 00:41:31 +0100

    Include line-awesome & primereact theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import "line-awesome/dist/line-awesome/css/line-awesome.min.css"
+import "primereact/resources/primereact.min.css"
+import "primereact/resources/themes/lara-light-indigo/theme.css"
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
